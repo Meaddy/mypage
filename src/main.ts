1e64b17244d1a3f538c64333baffb15079844b08
Meaddy <22281092@qq.com>
2025-03-30 15:24:14 +0800

    引入新的全局样式文件

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 import './uitls/global.css';  // 引入全局样式
-
+import './uitls/style.css'
 
 const app = createApp(App)
 
